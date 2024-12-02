67a050ef65b14160e7934ab6837a14e2da33492d
Akash Deep <adkeaesph720@gmail.com>
2024-12-02 22:35:38 +0530

    initial setup with a demo controller

---
+++ event-booking-service/src/main/java/com/planzo/event_booking_service/EventBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.planzo.event_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBookingServiceApplication.class, args);
+	}
+
+}
